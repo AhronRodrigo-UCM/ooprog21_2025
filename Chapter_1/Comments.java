cee763cc5de0bb31464bede4e633d6bbfeb57f76
AhronRodrigo-UCM <senyorito219@gmail.com>
2025-08-28 19:52:12 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,12 @@
+public class Comments {
+  public static void main(String[] args) {
+
+        // Upper comment
+
+        // Middle comment
+
+        // Lower comment
+
+        System.out.println("Hello World Comment ! hehe haha");
+    }
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("How I'm feeling, it doesn't matter");
+        System.out.println("Cause you know I'm okay");
+        System.out.println("Instead, I ask myself, \"Why do you worry?");
+        System.out.println("You know, you know I'm the same");
+    }
+}
