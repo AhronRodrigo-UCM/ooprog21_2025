e1363884f091ff72700bca44530712596428e3e2
AhronRodrigo-UCM <senyorito219@gmail.com>
2025-09-01 14:17:29 +0800

    Update FixDebugOne.java

--- Chapter_1/FixDebugOne.java
+++ Chapter_1/FixDebugOne.java
@@ -5,4 +5,14 @@ public class FixDebugOne
     {
         System.out.println("Hello World!");
     }
+
 }
+/*
+List down the spotted bugs below:
+e.g.
+
+1. Class names should not have spaces
+2. Java requires main in lowercase and String[] args (array)
+3. no E in " Systm "
+4. no semicolon at the end of System.out.println
+*/
